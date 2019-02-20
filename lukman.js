aa0aa417bb4a46b6700a70a576bb7694b73092f6
Fatai Balogun <fattylee.remod@gmail.com>
2019-02-20 10:42:13 +0100

    binary search algorithm implementation

--- lukman.js
+++ lukman.js
@@ -20,8 +20,25 @@ const binarySearch = (arr, n, x, searchFirst) => {
   return result;
 }
 
+// const sizeOfArr = arr.length;
+// const searchFirst = true;
+// console.log(binarySearch(arr, sizeOfArr, num, searchFirst));
+
+const findCount = (arr, num) => {
+  if (arr.indexOf(num) === -1) return 0;
+  
+  return (arr.lastIndexOf(num) - arr.indexOf(num) + 1);
+};
+
 const arr = [1,1,2,2,3,3,4,4,4];
-const sizeOfArr = arr.length;
-const num = 4;
-const searchFirst = true;
-console.log(binarySearch(arr, sizeOfArr, num, searchFirst));
+const num = 1;
+const count = findCount(arr, num);
+console.log(`Count of ${num} = ${count}`);
+
+
+
+
+
+
+
+
