5554c00a7524df62fa953c759113f4f9cade6230
Fatai Balogun <fattylee.remod@gmail.com>
2019-02-20 15:17:19 +0100

    add about route

--- app.js
+++ app.js
@@ -2,5 +2,20 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.send('<p>welcome to the best app</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.json({
+    message: 'This is about page',
+    body: {
+      name:  'Abu Adnaan',
+      age: 31,
+      gender: 'male'
+    }
+  });
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log('Server running on port', port));
