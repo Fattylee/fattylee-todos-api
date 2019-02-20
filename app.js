cd10acaebf178cb192341408ed9ab044645da9ac
Fatai Balogun <fattylee.remod@gmail.com>
2019-02-20 10:52:32 +0100

    setup express server

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-console.log(__dirname + '\\public');
-// console.log(__filename)
-// console.log(module);
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log('Server running on port', port));
