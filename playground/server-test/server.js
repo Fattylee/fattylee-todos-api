47a3fd598d370834d0704c3cf78fa64a47c691a2
Fatai Balogun <fattylee.remod@gmail.com>
2019-02-25 19:02:32 +0100

    test on GET /users route

--- playground/server-test/server.js
+++ playground/server-test/server.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.status(200).send('<h1>Server test app');
 });
 
+app.get('/users', (req, res) => {
+  res.status(200).send([
+  {
+    name: 'Abu Adnaan',
+    age: 31,
+  },
+  {
+    name: 'Lukman Dev',
+    age: 23,
+  }
+  ]);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
