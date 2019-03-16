64f352dad099939281d847a7ab9559834dee7304
Fatai Balogun <fattylee.remod@gmail.com>
2019-03-16 17:17:34 +0100

    async method: case 3

--- playground/promise.js
+++ playground/promise.js
@@ -16,19 +16,21 @@
  
  let data1;
  // promise call
- promise1()
-   .then( data => {
-     console.log('Promise 1 data:', data);
-     
+ const asyncMethod = async () => {
+   try {
+     const data = await promise1();
+     console.log('Promise 1 data resolved', data);
      //pass = false;
-     data1 = data;
-     return promise2(data)
-   })
-    .then( data2 => {
-      try { throw new Error('HI HERE');} catch(err) {console.log(err)}
-         console.log('Promise 2 data resolved',data1, data2);
-   })
-   .catch( err => console.log('ERROR:', err));
+    data1 = data;
+    const data2 = await promise2(data);
+    //throw new Error('HI HERE');
+    //try { throw new Error('HI HERE');} catch(err) {console.log(err)}
+    console.log('Promise 2 data resolved',data1, data2);
+  
+   }
+   catch( err ) { console.log('ERROR:', err)};
    
-   
+ }
+
+asyncMethod();
    
