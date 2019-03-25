ae7e99836083e1151ef0481e019821a2f61ccfde
Fatai Balogun <fattylee.remod@gmail.com>
2019-03-25 18:40:01 +0100

    practice test hook and destructuring

--- playground/destructuring.js
+++ playground/destructuring.js
@@ -12,11 +12,20 @@ const simpleObject = {
 };
 let a, b, cc, d;
 ({a, b, c: {cc : text}, c: {dd: {dog}}, d:[first, ...rest], c: {dd: {arr: [, age]}}, d: [,,,,[, {sex=78}]]} = simpleObject);
-console.log('a:', a, 'b:', b, 'cc:', cc, 'text:', text, 'dog:', dog, 'd:', first, rest, 'second:', age, 'sex:', sex);
+//console.log('a:', a, 'b:', b, 'cc:', cc, 'text:', text, 'dog:', dog, 'd:', first, rest, 'second:', age, 'sex:', sex);
 
 const str = 'random string what is it';
 const regex = /(.*)[\s]+(is)[\s]+([\w]+)/i;
 
 let result = regex.exec(str);
 
-console.log('result:', result);
+//console.log('result:', result);
+let userPayload = {
+  name: 'Abu Adnaan',
+  man: function() {
+    return (userPayload.name)
+  },
+
+}
+
+console.log(userPayload.man());
