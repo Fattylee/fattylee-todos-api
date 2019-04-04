c047b38b3ff179734ce78a95788f4ceb5d3f8758
Fatai Balogun <fattylee.remod@gmail.com>
2019-04-04 16:00:32 +0100

    refactor circleci testing

--- server/config/config.js
+++ server/config/config.js
@@ -12,9 +12,4 @@ else if (env === 'test') {
   process.env.MONGOLAB_URI = 'mongodb://127.0.0.1/TodoAppTest';
 }
 
-else if(env === 'staging') {
-  //process.env.PORT = 8080;
-  //process.env.MONGOLAB_URI = process.env.TEST_DB;
-}
-
 module.exports = env;
